b7643a625a9dd0f0f0e3c730f16e81bb2275fc64
PhilippFaller <philipp.faller@yahoo.de>
2015-04-23 14:59:53 +0200

    main loop + first attempts for castle

--- easyStrategy.js
+++ easyStrategy.js
@@ -1,2 +1,54 @@
-var g = document.getElementById("canvas").getContext("2d");
-g.fillText("Hello World", 0, 10);
+//Globals
+var g = document.getElementById("canvas").getContext("2d"); //graphic context
+var frameRequest;	//saves id of the frame request to the window
+var objects = [];
+
+//functions
+function main(){
+	//intit
+	var v = new Castle(10, 10, 0);
+	objects.push();
+	console.log(v.x);
+	v.render(0);
+	//start loop
+	loop();
+}
+
+function loop(deltaT) {
+    frameRequest = window.requestAnimationFrame(loop);		//request next frame
+
+    update(deltaT); 
+    render(deltaT);	
+ }
+ 
+ function update(deltaT){
+	 /*for(o in objects){
+		 o.update();
+	 }*/
+ }
+ 
+ function render(deltaT){
+	 for(o in objects){
+		 o.render(deltaT);
+	 }
+ }
+ 
+ // classes
+ function Castle(x, y, owner){
+	 this.x = x;
+	 this.y = y;
+	 this.owner = owner;
+	 this.width = 200;
+	 this.height = 200;
+
+	 this.update = function(){console.log("salle");};
+	 this.render = function(deltaT){
+		 console.log("hi");
+		 g.fillColor = "red";
+		 g.beginPath();
+		 g.fillRect(this.x, this.y, this.width, this.height);
+		 g.stroke();
+	 };
+ }
+ 
+ main();
