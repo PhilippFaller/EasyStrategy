ed26943a3381f50322c4c672e02557320850919d
PhilippFaller <philipp.faller@yahoo.de>
2015-04-27 11:34:13 +0200

    added vector class. fixed syntax

--- easyStrategy.js
+++ easyStrategy.js
@@ -6,131 +6,144 @@ var objects = [];
 var selectedObject;
 
 //functions
-function main(){
+function main() {
 	//intit
-	objects.push(new Castle(10, 10, 0));
-	objects.push(new SwordFighter(300, 300, 0));
+	objects.push(new Castle(new Vector(10, 10), 0));
+	objects.push(new SwordFighter(new Vector(300, 300), 0));
 	//start loop
 	loop();
 }
 
 function loop(deltaT) {
-    frameRequest = window.requestAnimationFrame(loop);		//request next frame
+	frameRequest = window.requestAnimationFrame(loop);		//request next frame
 
-    update(deltaT); 
-    render(deltaT);	
- }
+	update(deltaT); 
+	render(deltaT);	
+}
  
- function update(deltaT){
-	objects.forEach(function(o){o.update(deltaT)});
- }
+function update(deltaT) {
+	objects.forEach(function(o){ o.update(deltaT) });
+}
  
- function render(deltaT){
+function render(deltaT) {
 	g.fillStyle = "green";
 	g.fillRect(canvas.offsetLeft, canvas.offsetTop, canvas.width, canvas.height);
-	objects.forEach(function(o){o.render(deltaT)});
- }
+	objects.forEach(function(o){ o.render(deltaT) });
+}
 
-canvas.addEventListener("click", function (event){
-		var x = event.clientX - canvas.offsetLeft; 
-		var y = event.clientY - canvas.offsetTop;
+canvas.addEventListener("click", function (event) {
+	// var x = event.clientX - canvas.offsetLeft; 
+	// var y = event.clientY - canvas.offsetTop;
+	var pos = new Vector(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
 
-		objects.forEach(function(o){
-			//checks wheter clicked or not
-			if(x > o.x && x < o.x + o.width && y > o.y && y < o.y + o.height){
-				o.isSelected = true;
-				selectedObject = o;
+	objects.forEach(function(o) {
+		//checks whether clicked or not
+		if(pos.x > o.pos.x && pos.x < o.pos.x + o.width && pos.y > o.pos.y && pos.y < o.pos.y + o.height) {
+			o.isSelected = true;
+			selectedObject = o;
+		}
+		else
+		{
+			if(event.ctrlKey) {//control button 
+				o.goalX = pos.x;
+				o.goalY = pos.y;
+			} 
+			else {
+				o.isSelected = false;
+				selectedObject = undefined;
 			}
-			else{
-				if(event.ctrlKey){ //middle mouse button
-					o.goalX = x;
-					o.goalY = y;
-				} 
-				else {
-					o.isSelected = false;
-					selectedObject = undefined;
-				}
-			}
-		});
+		}
+	});
 
-	}
-		
-);
+});
 	
  
  // classes
- function Castle(x, y, owner){
-	 this.x = x;
-	 this.y = y;
-	 this.owner = owner;
-	 this.width = 200;
-	 this.height = 200;
-	 this.isSelected = false;
+function Vector(x, y) {
+	this.x = x;
+	this.y = y;
+	this.add = function(v) {
+		return new Vector(this.x + v.x, this.y + v.y);
+	};
+	this.sub = function(v) {
+		return new Vector(this.x - v.x, this.y - v.y);
+	};
+	this.norm = function() {
+		return Math.sqrt(x*x + y*y);
+	};
+}	 
+ 
+function Castle(pos, owner) {
+	this.pos = pos;
+	this.owner = owner;
+	this.width = 200;
+	this.height = 200;
+	this.isSelected = false;
 
-	 this.update = function(){};
-	 this.render = function(deltaT){
-		 g.clearRect(this.x, this.y, this.width, this.height);
-		 g.fillStyle = "black";
-		 g.beginPath();
-		 g.fillRect(this.x, this.y, this.width, this.height);
-		 if(this.isSelected){
-			 g.fillStyle = "red";
-			 g.fillRect(this.x, this.y, this.width, this.height);
-		 }
-		 g.stroke();
-	 };
- }
-
-function Barrack(x, y, owner){
-		 this.x = x;
-	 this.y = y;
-	 this.owner = owner;
-	 this.width = 100;
-	 this.height = 100;
-	 this.isSelected = false;
-	 this.update = function(){};
-	 this.render = function(deltaT){
-		 g.fillStyle = "black";
-		 g.clearRect(this.x, this.y, this.width, this.height);
-		 g.beginPath();
-		 g.fillRect(this.x, this.y, this.width, this.height);
-		 if(this.isSelected){
-			 g.fillStyle = "red";
-			 g.fillRect(this.x, this.y, this.width, this.height);
-		 }
-		 g.stroke();
-	 };
+	this.update = function(){};
+	this.render = function(deltaT) {
+		g.clearRect(this.pos.x, this.pos.y, this.width, this.height);
+		g.fillStyle = "black";
+		g.beginPath();
+		g.fillRect(this.pos.x, this.pos.y, this.width, this.height);
+		if(this.isSelected)
+		{
+			g.fillStyle = "red";
+			g.fillRect(this.pos.x, this.pos.y, this.width, this.height);
+		}
+		g.stroke();
+	};
 }
 
-function SwordFighter(x, y, owner){
-	 this.x = x;
-	 this.y = y;
-	 this.owner = owner;
-	 this.width = 10;
-	 this.height = 10;
-	 this.isSelected = false;
-	 this.goalX = x;
-	 this.goalY = y;
-	 //this.speed = 2;
-	 this.update = function(){
-		if(this.x < this.goalX) this.x++;
-		else if(this.x > this.goalX) this.x--;
-		if(this.y < this.goalY) this.y++;
-		else if(this.y > this.goalY) this.y--; 
-	 };
-	 this.render = function(deltaT){
-		 g.fillStyle = "black";
-		 g.clearRect(this.x, this.y, this.width, this.height);
-		 g.beginPath();
-		 g.fillRect(this.x, this.y, this.width, this.height);
-		 if(this.isSelected){
-			 g.fillStyle = "red";
-			 g.fillRect(this.x, this.y, this.width, this.height);
-		 }
-		 g.stroke();
-	 };
+function Barrack(pos, owner) {
+	this.pos = pos;
+	this.owner = owner;
+	this.width = 100;
+	this.height = 100;
+	this.isSelected = false;
+	this.update = function(){};
+	this.render = function(deltaT) {
+		g.fillStyle = "black";
+		g.clearRect(this.pos.x, this.pos.y, this.width, this.height);
+		g.beginPath();
+		g.fillRect(this.pos.x, this.pos.y, this.width, this.height);
+		if(this.isSelected) {
+			g.fillStyle = "red";
+			g.fillRect(this.pos.x, this.pos.y, this.width, this.height);
+		}
+		g.stroke();
+	};
+}
+
+function SwordFighter(pos, owner) {
+	this.pos = pos;
+	this.owner = owner;
+	this.width = 10;
+	this.height = 10;
+	this.isSelected = false;
+	this.goalX = pos.x;
+	this.goalY = pos.y;
+	//this.speed = 2;
+	this.update = function() {
+		if(this.pos.x < this.goalX) this.pos.x++;
+		else if(this.pos.x > this.goalX) this.pos.x--;
+		if(this.pos.y < this.goalY) this.pos.y++;
+		else if(this.pos.y > this.goalY) this.pos.y--; 
+	};
+	this.render = function(deltaT) {
+		g.fillStyle = "black";
+		g.clearRect(this.pos.x, this.pos.y, this.width, this.height);
+		g.beginPath();
+		g.fillRect(this.pos.x, this.pos.y, this.width, this.height);
+		if(this.isSelected)
+		{
+			g.fillStyle = "red";
+			g.fillRect(this.pos.x, this.pos.y, this.width, this.height);
+		}
+		g.stroke();
+	};
 }
  
- main();
+main();
 
 
